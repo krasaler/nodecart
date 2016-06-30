000a3c19a9c872834afd97e8264708f30f3ff0e4
Alexandr Krasny <krasaler@gmail.com>
2016-06-30 22:14:35 +0300

    Related options

--- app/model/catalog/product.js
+++ app/model/catalog/product.js
@@ -1,13 +1,22 @@
 var bookshelf = require('../base');
 
 var Product = bookshelf.Model.extend({
-  tableName: 'oc_product'
+  tableName: 'product',
+  product_option: function() {
+    return this.hasMany(ProductOption);
+  }
+});
+var ProductOption = bookshelf.Model.extend({
+  tableName: 'product_option',
+  product: function() {
+    return this.belongsTo(Product,'product_id');
+  }
 });
 
 function * getProducts(){
 	var result = [];
 
-	yield Product.fetchAll()
+	yield Product.fetchAll({withRelated: ['product_option']})
 		.then(function (products) {
 			 result = products.toJSON();
 		}).catch(function(err) {
@@ -16,10 +25,9 @@ function * getProducts(){
 
 	return result;
 }
-
 function * getProduct(product_id){
 	var result = [];
-	
+
 	yield Product.where('product_id', product_id).fetch()
 		.then(function (product) {
 			 result = product.toJSON();
