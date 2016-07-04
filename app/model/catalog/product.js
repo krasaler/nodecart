1976bb621c61fbd64d193f4cdfb960bf07afe8d9
Alexandr Krasny <krasaler@gmail.com>
2016-07-04 21:37:08 +0300

    fix

--- app/model/catalog/product.js
+++ app/model/catalog/product.js
@@ -4,6 +4,15 @@ var Product = bookshelf.Model.extend({
   tableName: 'product',
   product_option: function() {
     return this.hasMany(ProductOption);
+  },
+  categories: function() {
+    return this.hasMany(Categories);
+  },
+  descriptions: function() {
+    return this.hasMany(ProductDescriptions);
+  },
+  rewards:function(){
+    return this.hasMany(ProductRewards);
   }
 });
 var ProductOption = bookshelf.Model.extend({
@@ -12,11 +21,29 @@ var ProductOption = bookshelf.Model.extend({
     return this.belongsTo(Product,'product_id');
   }
 });
+var ProductDescriptions = bookshelf.Model.extend({
+  tableName: 'product_description',
+  product: function() {
+    return this.belongsTo(Product,'product_id');
+  }
+});
+var ProductRewards = bookshelf.Model.extend({
+  tableName: 'product_reward',
+  product: function() {
+    return this.belongsTo(Product,'product_id');
+  }
+});
+var Categories = bookshelf.Model.extend({
+  tableName: 'product_to_category',
+  product: function() {
+    return this.belongsTo(Product,'product_id');
+  }
+});
 
 function * getProducts(){
 	var result = [];
 
-	yield Product.fetchAll({withRelated: ['product_option']})
+	yield Product.fetchAll({withRelated: ['product_option','categories','descriptions','rewards']})
 		.then(function (products) {
 			 result = products.toJSON();
 		}).catch(function(err) {
@@ -28,7 +55,7 @@ function * getProducts(){
 function * getProduct(product_id){
 	var result = [];
 
-	yield Product.where('product_id', product_id).fetch()
+	yield Product.where('id', product_id).fetch({withRelated: ['product_option','categories','descriptions','rewards']})
 		.then(function (product) {
 			 result = product.toJSON();
 		}).catch(function(err) {
